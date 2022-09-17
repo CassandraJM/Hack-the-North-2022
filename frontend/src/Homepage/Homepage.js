61f0da5ff5822a1212955dd54cec985e13da367e
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2022-09-17 16:37:09 -0400

    remove grid

--- frontend/src/Homepage/Homepage.js
+++ frontend/src/Homepage/Homepage.js
@@ -1,22 +1,29 @@
-import React from 'react'
-import './Homepage.css'
+import React from 'react';
+import './Homepage.css';
+import { useState } from 'react';
+const App = () => {
 
-export const App = () => {
+  {/* word input rendering */}
+  //const [fieldInput, setFieldInput] = useState("");
+  const [fieldInput, setFieldInput] = useState("");
+
+
   return (
-    <div>
+    <div className='homepage_container'>
       {/* Title */}
       <div className="page_header">
-        <h1 className="page_name">ThinkNote++</h1>
+        <h1 className="page_name">ThinkBoard</h1>
       </div>
 
-      {/* Textbox Form */}
-      <form>
+      {/* Input Form */}
+      <form className='inputForm'>
         <label>
-          <input type="text" name="name" placeholder="Enter your ideas here" />
+        <input type="submit" value="Submit"/>
+          <input className="textField" type="text" placeholder="Enter your ideas here" value={fieldInput} onChange={event => setFieldInput(event.target.value)}/>
         </label>
-        <input type="submit" value="Submit" />
       </form>
-    </div>
+
+    </div>    
   )
 }
 
