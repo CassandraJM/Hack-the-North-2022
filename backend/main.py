cad6eed68cb7170719f316f75ef5af4928b6a422
Rohit Prakash <rohit.prakash@mail.utoronto.ca>
2022-09-17 15:28:52 -0400

    adding expand and create backend

--- backend/main.py
+++ backend/main.py
@@ -1,51 +1,55 @@
 from flask import Flask
+from flask import request
+import pandas as pd
 
 app = Flask(__name__)
 
-all_ideas = {str, [str, [(str, int)], str]}
+all_ideas = {str: [str, [(str, int)], str]}
 COMMAND_CREATE = 'Create'
 COMMAND_EXPAND = 'Expand'
 COMMAND_SHRINK = 'Shrink'
 COMMAND_ERROR = 'Error'
+CHILD_BUBBLES = 0
+PREDICTIONS = 1
+IMAGES = 2
+
+###
+# {
+#     input: string,
+#     parent: string,
+# }
+###
 
 
-@app.route('/<string:new_input>', methods=['POST'])
-def process_human_input(new_input):
-    text = new_input
+@app.route('/api/create', methods=['POST'])
+def process_human_input():
+    new_input = request.json['input']
+    parent = request.json['parent']
     command_word = _get_command_word(text)
-    if text[:6].casefold() == COMMAND_CREATE.casefold():
-        # send text[6:] to model
-        # add bubble with summary, related queries
-        # add summary to all_human_bubbles
-        # make new bubble current point
-        pass
-    elif text[:6].casefold() == COMMAND_EXPAND.casefold():
-        if text[4:] in all_human_bubbles:
-            # make bubble current point
-            pass
-        else:
-            pass
-    elif text[:len(COMMAND_SHRINK)].casefold() == COMMAND_SHRINK.casefold():
-        if text[len(COMMAND_SHRINK):] not in all_human_bubbles:
-            # add new bubble to origin, generate summary, related queries
-            pass
-        else:
-            pass
-    else:
-        # incorrect command word
-        pass
+    df = get_summary(new_input)
+    summary = df.loc[0]
+    counter = 1
+    while summary in all_ideas:
+        summary = df.loc[counter]
+        counter += 1
+    prediction = get_prediction(summary)
+    all_ideas[summary] = [parent, [(PREDICTIONS, prediction)], new_input]
+    return {summary, prediction}
 
 
-def _get_command_word(text):
-    if text[:6].casefold() == COMMAND_CREATE.casefold():
-        return COMMAND_CREATE
-    elif text[:4].casefold() == COMMAND_MOVE.casefold():
-        return COMMAND_MOVE
-    elif text[:len(COMMAND_BRANCH)].casefold() == COMMAND_BRANCH.casefold():
-        return COMMAND_BRANCH
-    else:
-        return COMMAND_ERROR
-
+###
+# {
+#     parent: string,
+# }
+###
+@app.route('/api/expand', methods=['POST'])
+def expand():
+    parent = request.json['parent']
+    parents_children = all_ideas[parent][1]
+    grandchildren = {} #{str: [(int, str)]}
+    for child in parents_children: #child is a tuple (type, child name)
+        grandchildren[child[1]] = all_ideas[child[1]][1]
+    return grandchildren
 
 if __name__ == '__main__':
     app.run()
